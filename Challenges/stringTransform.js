e8c258fda21db6bb83e0a51dabe47e671e730c69
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-06-07 19:54:33 +0200

    string transformation challenge

--- Challenges/stringTransform.js
+++ Challenges/stringTransform.js
@@ -1,11 +1,31 @@
-function transformString(str) {
-    const len = str.length;
-    if (len % 15 === 0) {
-        return str.split('').reverse().map(char => char.charCodeAt(0)).join(' ');
-    } else if (len % 3 === 0) {
-        return str.split('').reverse().join('');
-    } else if (len % 5 === 0) {
-        return str.split('').map(char => char.charCodeAt(0)).join(' ');
+function transformString(text) {
+    const reverseString = str => str.split("").reverse().join("");
+    const toAscii = str => str.split('').map(char => char.charCodeAt(0)).join(' ');
+
+    switch (true) {
+        case (text.length % 3 === 0 && text.length % 5 === 0):
+            text = reverseString(text);
+            text = toAscii(text);
+            break;
+        case (text.length % 3 === 0):
+            text = reverseString(text);
+            break;
+        case (text.length % 5 === 0):
+            text = toAscii(text);
+            break;
     }
-    return str;
+    return text;
 }
+
+// Example usage
+const text = "Helloowrtyftsiu";
+console.log(transformString(text));  // Output: 117 105 115 116 102 116 121 114 119 111 108 108 101 72
+
+const text2 = "Helloo";
+console.log(transformString(text2));  // Output: oolleH
+
+const text3 = "World";
+console.log(transformString(text3));  // Output: 87 111 114 108 100
+
+const text4 = "Hello world";
+console.log(transformString(text4));  // Output: Hello world
