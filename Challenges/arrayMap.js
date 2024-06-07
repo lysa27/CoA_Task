0bc016c2d3c05396e13b8d72c7131949542f6bb1
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-06-07 19:03:09 +0200

    add challenge

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,28 @@
+function findSubarray(array, target) {
+    let sum = 0;
+    let start = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        
+        while (sum > target && start <= i) {
+            sum -= array[start];
+            start++;
+        }
+
+        if (sum === target) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Sample tests
+const arr1 = [4, 2, 7, 1, 9, 5];
+const target1 = 10;
+console.log(findSubarray(arr1, target1)); // Output: true
+
+const arr2 = [1, 2, 3, 4, 5];
+const target2 = 10;
+console.log(findSubarray(arr2, target2)); // Output: true
